de2886e752afc0dea64c97b35c5283627df5eade
Ade Setiawan <adesetiawan0675@gmail.com>
2022-11-19 20:38:54 +0700

    add components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <div>
+        <h1>My profile, username is:Profile</h1>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
